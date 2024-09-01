b265a8fcd550afdde697fa72c78b38bf7aff7bd8
TaeWook Park <ld5ehom@gmail.com>
2024-08-31 23:43:02 -0700

    task 2 login init
    task 2 login Packages (firebase, aws-amplify) download

--- Store-ios/AppDelegate.swift
+++ Store-ios/AppDelegate.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    // Firebase
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Firebase
+        FirebaseApp.configure()
+        
         return true
     }
 
